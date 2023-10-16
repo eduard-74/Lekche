30c85bd44929c61d0dbfc479a8aac8312fd0e51d
eduard74 <lipineduard@yandex.ru>
2023-10-17 01:33:15 +0400

    вторая программа

---
+++ lekche002_HelloUser/Program.cs
@@ -0,0 +1,14 @@
+﻿// пишем 2ю прогу
+пишет в новой строчке
+Console.WriteLine("Введите Ваше Имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+
+
+//выводит в одну строчку
+Console.Write("Введите Ваше Имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
